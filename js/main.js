fbb319bc6753429820a3a3470dc5e4f0cf34d085
mostafa93sh <mostafa.khalel.93@gmail.com>
2023-01-17 22:37:00 +0200

    change images and complete functionality of slider

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const imageContainerArray = Array.from(
   document.querySelector(".images-container").children,
 );
 const imagesContainer = document.querySelector(".images-container");
-let index = 1;
 
 // positionOfContainer.addEventListener("animationiteration", (e) => {
 //   console.log("e");
@@ -12,25 +11,32 @@ let index = 1;
 
 // const bulletsItemHandler = () => {};
 imageContainerArray.forEach((img, i) => img.setAttribute("data-index", i));
-const newListBullets = document.createElement("ul");
+// const newListBullets = document.createElement("ul");
 const slidesNumber = imageContainerArray.length;
-for (let i = 1; i <= slidesNumber; i++) {
-  const newListBulletsElement = document.createElement("li");
-  newListBulletsElement.setAttribute("data-index", i);
-  newListBulletsElement.appendChild(document.createTextNode(i));
-  newListBullets.appendChild(newListBulletsElement);
-}
-document.querySelector("main.container").appendChild(newListBullets);
+// for (let i = 1; i <= slidesNumber; i++) {
+//   const newListBulletsElement = document.createElement("li");
+//   newListBulletsElement.setAttribute("data-index", i);
+//   newListBulletsElement.appendChild(document.createTextNode(i));
+//   newListBullets.appendChild(newListBulletsElement);
+// }
+// document.querySelector("main.container").appendChild(newListBullets);
 const listItemArray = document.querySelectorAll(`ul li`);
+const listItemLength = listItemArray.length;
+
+let index = 0;
 setInterval(() => {
-  if (index < 3) {
-    imagesContainer.style.left = `-${index}000px`;
+  if (index < 2) {
+    imagesContainer.style.left = `-${index + 1}000px`;
+
+    listItemArray[index].classList.remove("active");
+    listItemArray[index + 1].classList.add("active");
+
     // const dataIndex = imageContainerArray[index].getAttribute("data-index");
-    listItemArray[index - 1].classList.add("active");
     index++;
   } else {
     listItemArray.forEach((el) => el.classList.remove("active"));
-    index = 1;
+    index = 0;
+    listItemArray[index].classList.add("active");
     imagesContainer.style.left = `0`;
   }
 }, 4000);
